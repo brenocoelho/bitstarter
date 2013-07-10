3aa79d600e4d95346c130eff10b0ae2f4544ec0f
Breno Coelho <breno.coelho@trusoll.com>
2013-07-10 14:17:48 -0300

    Hello World from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	var file = fs.readFileSync('index.html');
+	console.log(file);
+	var buffer = new Buffer(file)
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
